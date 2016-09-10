10b914814be16a6aa1ad5f9be163fb66edf71104
Joseph Cox <josephmcox@gmail.com>
2016-09-09 23:00:23 -0400

    Added all the passport code for user system and temp pages for login/signup. All passwords are hashed for security using bcrypt

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://dev:9P7BWF==^nH!g7x_@ds029106.mlab.com:29106/ggcinternapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
