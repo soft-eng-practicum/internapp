72e7cc34fa567e25182fa4e22348fe3d7c45f542
jcanguyen <jnguyen7@ggc.edu>
2016-09-19 00:03:23 -0400

    Added route to ITECH page

--- app/routes.js
+++ app/routes.js
@@ -45,11 +45,21 @@ module.exports = function(app, passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile', isLoggedIn, function(req, res) {
+    app.get('/profile',, isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
+
+    // =====================================
+    // ITECH ==============================
+    // =====================================
+    app.get('/ITECH', isLoggedIn, function(req, res) {
+        res.render('ITECH.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
 
     // =====================================
     // LOGOUT ==============================
