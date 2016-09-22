6343204dd981bbd0d1731381fe4189fabc12d4f7
Joseph Cox <josephmcox@gmail.com>
2016-09-22 01:24:34 +0000

    Added dashboard instead of profile

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ module.exports = function(app, passport) {
 
     // process the login form
       app.post('/login', passport.authenticate('local-login', {
-          successRedirect : '/profile', // redirect to the secure profile section
+          successRedirect : '/dashboard', // redirect to the secure profile section
           failureRedirect : '/login', // redirect back to the signup page if there is an error
           failureFlash : true // allow flash messages
       }));
@@ -36,17 +36,17 @@ module.exports = function(app, passport) {
 
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/dashboard', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
     // =====================================
-    // PROFILE SECTION =====================
+    // DASHBOARD SECTION =====================
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
+    app.get('/dashboard', isLoggedIn, function(req, res) {
+        res.render('dashboard.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
