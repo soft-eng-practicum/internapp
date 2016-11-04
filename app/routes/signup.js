e2b16a49eb51b8cdfe6ea98a8f36028a72bd77d8
Joseph Cox <josephmcox@gmail.com>
2016-11-04 01:59:02 +0000

    Added edit user info/profile

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -22,6 +22,44 @@ module.exports = function(app, passport) {
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
+    
+        app.get('/editprofile', isLoggedIn, function(req, res) {
+            User.findOne({
+                    'local.email': req.user.email
+                }, function(err, profile) {
+                    res.render('editprofile.ejs', {
+                        profiledetails: profile,
+                        user: profile,
+                         message: req.flash('info') 
+                    });
+                });
+    });
+    
+            app.post('/editprofile', function(req, res) {
+                console.log(req.user.email);
+        // render the page and pass in any flash data if it exists
+    User.update({'local.email': req.user.email}, {
+        'local.studentid': req.body.studentid,
+        'local.fname': req.body.fname,
+        'local.lname': req.body.lname,
+        'local.address': req.body.address,
+        'local.city': req.body.city,
+        'local.state': req.body.state,
+        'local.zipcode': req.body.zipcode,
+        'local.discipline': req.body.discipline
+    }, function (err) {
+  if(err) {
+      
+      req.flash('info',err);
+      res.redirect('/editprofile');}
+  else{
+      req.flash('info','success!');
+        res.redirect('/editprofile');
+    }
+    });
+            });
+    
+    
 };
 
 // route middleware to make sure a user is logged in
