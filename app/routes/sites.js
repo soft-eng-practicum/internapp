d598f21aed889393ee8c275349172cac638aa8cb
jcanguyen <jnguyen7@ggc.edu>
2016-11-15 20:08:50 -0500

    changed requirements for MOU, added sites into database

--- app/routes/sites.js
+++ app/routes/sites.js
@@ -16,14 +16,14 @@ module.exports = function(app, passport) {
    res.render('site.ejs', {
             siteList : sites,
             user : req.user
-        }); 
+        });
 });
         }
         else{
-           res.redirect('/dashboard'); 
+           res.redirect('/dashboard');
         }
     });
-    
+
         app.get('/site/:siteid', isLoggedIn, function(req, res) {
           Site.findOne({ _id: req.params.siteid },function (err, sitedetail) {
   if (err) {
@@ -33,12 +33,12 @@ module.exports = function(app, passport) {
             site : sitedetail,
             user : req.user,
             message : req.flash('info')
-        }); 
+        });
   }
 
 });
     });
-    
+
             app.get('/site/edit/:siteid', isLoggedIn, function(req, res) {
           Site.findOne({ _id: req.params.siteid },function (err, sitedetail) {
   if (err) {
@@ -48,12 +48,12 @@ module.exports = function(app, passport) {
             site : sitedetail,
             user : req.user,
             message : req.flash('info')
-        }); 
+        });
   }
 
 });
     });
-    
+
             app.post('/site/:siteid', isLoggedIn, function(req, res) {
           Site.update({ _id: req.params.siteid },{$push: {"contacts": {name: req.body.name, phone: req.body.phone}}},function (err) {
   if (err){
@@ -66,9 +66,9 @@ module.exports = function(app, passport) {
 
     });
      });
-     
+
                  app.post('/site/edit/:siteid', isLoggedIn, function(req, res) {
-          Site.update({ _id: req.params.siteid },{name: req.body.name, address: req.body.address, city: req.body.city, state: req.body.state, zipcode: req.body.zipcode, 
+          Site.update({ _id: req.params.siteid },{name: req.body.name, address: req.body.address, city: req.body.city, state: req.body.state, zipcode: req.body.zipcode,
          mou: req.body.mou, mouexpiration: req.body.mouexpiration},function (err) {
   if (err){
          req.flash('info',err);
@@ -80,7 +80,7 @@ module.exports = function(app, passport) {
 
     });
      });
-     
+
                       app.post('/site/delete/:siteid', isLoggedIn, function(req, res) {
           Site.remove({ _id: req.params.siteid },function (err) {
   if (err){
@@ -93,24 +93,24 @@ module.exports = function(app, passport) {
 
     });
      });
-     
+
         app.get('/addsite', isLoggedIn, function(req, res) {
         if(true){
           res.render('addsite.ejs', {
             user : req.user,
             messages: req.flash('info')
-        });   
+        });
         }
         else{
-           res.redirect('/dashboard'); 
+           res.redirect('/dashboard');
         }
     });
-    
+
      app.post('/addsite', isLoggedIn, function(req, res,next){
-         var site = new Site({ name: req.body.name, address: req.body.address, city: req.body.city, state: req.body.state, zipcode: req.body.zipcode, 
+         var site = new Site({ name: req.body.name, address: req.body.address, city: req.body.city, state: req.body.state, zipcode: req.body.zipcode,
          mou: req.body.mou, mouexpiration: req.body.mouexpiration });
 site.save(function (err) {
-  if (err) 
+  if (err)
   {
       req.flash('info', err)
       res.render('addsite.ejs', {
