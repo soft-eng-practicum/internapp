ae1061b2a870bc91d97ce27cf0eb47a608b02028
Robert <rbryan21@gmail.com>
2017-02-09 11:14:21 -0500

    Cleaned up folder structure, renamed requirements doc, cleaned up some comments in server.js

--- server.js
+++ server.js
@@ -8,20 +8,15 @@ var port     = process.env.PORT || 8000;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
-
 var configDB = require('./config/database.js');
-
-
 var routes = require('./app/routes/index');
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
-
 require('./config/passport'); 
 
 // set up our express application
@@ -38,25 +33,7 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-//require('./app/routes/index')(app, passport);
 require('./app/routes/index.js')(app, passport);
-
-
-
-// routes ======================================================================
-/*require('./app/routes/applications.js')(app, passport); // load our routes and pass in our app and fully configured passport
-require('./app/routes/dashboard.js')(app, passport);
-require('./app/routes/home.js')(app, passport);
-require('./app/routes/login.js')(app, passport);
-require('./app/routes/logout.js')(app, passport);
-require('./app/routes/promote.js')(app, passport);
-require('./app/routes/signup.js')(app, passport);
-require('./app/routes/sites.js')(app, passport);
-require('./app/routes/forgot.js')(app, passport);
-require('./app/routes/reset.js')(app, passport);
-require('./app/routes/faq.js')(app, passport); 
-*/
-
 
 // launch ======================================================================
 app.listen(port);
