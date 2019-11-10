6c18399fe4ca75f39bfda9780a7d29a129ef0026
ekim22 <ekim2@ggc.edu>
2019-11-10 15:50:08 -0500

    Upload is working

--- server.js
+++ server.js
@@ -4,7 +4,9 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
+var bodyParser = require('body-parser');
 var path     = require('path');
+var crypto = require('crypto');
 var port     = process.env.PORT || 8000;
 var mongoose = require('mongoose');
 var passport = require('passport');
@@ -21,6 +23,8 @@ var GridFsStorage = require('multer-gridfs-storage');
 var Grid = require('gridfs-stream');
 var fs  = require('fs');
 
+var methodOverride = require('method-override');
+
 //New GridFS Stuff
 var User = require('./app/models/user');
 var Document = require('./app/models/document');
@@ -32,14 +36,16 @@ var multiparty = require('connect-multiparty')();
 // configuration ===============================================================
 const mongoURI = 'mongodb://meraki:$oftdev2ELKJJ@ds259732.mlab.com:59732/ggcinternapp';
 
-const connection = mongoose.connect(configDB.url);
+const connection = mongoose.connect(mongoURI);
 const conn = mongoose.createConnection(configDB.url);
 require('./config/passport'); 
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 var mongoDriver = mongoose.mongo;
 
@@ -104,32 +110,117 @@ let gfs;
 
 conn.once('open', () => {
     gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('File Uploads');
 });
 
-const  storage = new GridFsStorage({
+// const storage = new GridFsStorage({
+//     url: mongoURI,
+//     file: function(req, file) {
+//         //     // if (file.mimeType === 'application/pdf') {
+//         //     //     console.log("Got application/pdf");
+//         //     //     return {
+//         //     //         filename: 'pdffile' + Date.now()
+//         //     //     };
+//         //     // }
+//         //     // else if (file.mimeType === 'text/plain') {
+//         //     //     console.log("Got text/plain");
+//         //     //     return {
+//         //     //         filename: 'textfile' + Date.now()
+//         //     //     };
+//         //     // }
+//         //     // else if (file.contentType === 'text/plain') {
+//         //     //     console.log("Got text/plain from content type");
+//         //     //     return {
+//         //     //         filename: 'contexttextfile' + Date.now()
+//         //     //     };
+//         //     // }
+//         //     // else {
+//         //     //     console.log("I'm returning null...");
+//         //     //     return 'EKTESTFILENAME_' + Date.now();
+//         //     // }
+//         //     // return {filename:'EKTESTFILENAME_' + Date.now()};
+//         //     return 'file_' + Date.now();
+//     }
+// });
+
+let storage = new GridFsStorage({
     url: mongoURI,
-    file: function(req, file){
-        return{
-             filename: 'file' + Date.now(),
-             metadata: req.body
-        };
+    file: (req, file) => {
+        return {filename: 'TESTING SIMPLE RETURN1'};
+        {
+            return 'TESTING SIMPLE RETURN2';
+            return new Promise((resolve, reject) => {
+                crypto.randomBytes(16, (err, buf) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    const filename = file.originalname + path.extname(file.originalname);
+                    const fileInfo = {
+                        filename: 'TESTING UPLOADS',
+                        bucketName: 'uploads'
+                    };
+                    resolve(fileInfo);
+                });
+            });
+        }
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = file.originalname + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: 'TESTING UPLOADS',
+                    bucketName: 'uploads'
+                };
+                resolve(fileInfo);
+            });
+        });
+        //
+        // if (file.mimetype === 'image/jpeg') {
+        //
+        //     return {
+        //         bucketName: 'photos',
+        //         filename: file.originalName
+        //     };
+        // } else {
+        //     return '';
+        // }
     }
 });
 
+let upload = multer ({ storage });
 
-const upload = multer ({ storage });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set favicon to the SST crest
+app.use(favicon(__dirname + '/public/images/logo.png'));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'dfgd5155435445df1gdfgdry5y4345' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.post('/uploadTest', upload.single('file'), (req, res) => {
-
+    res.json({ file: req.file.grid });
+    console.log(req.file.grid);
     //Test document object to see if you can add a document from inside
-    //this function.  It works.  
+    //this function.  It works.
+
     var document = new Document({
         'user' : {
-            'user_id' : 'sdf',
-            'fname': 'JOrdan',
-            'lname': 'VINCENT',
-            'user_email': 'testemail@gmail.com'
+            // 'user_id' : req.user.studentid,
+            // 'user_id' : req.user.studentid,
+            // 'fname': req.user.fname,
+            // 'lname': req.user.lname,
+            // 'user_email': req.user.email
+            'user_id' : req.file.filename,
+            'fname': req.file.originalname,
+            'lname': req.file.grid.contentType,
+            'user_email': req.file.grid.uploadDate
         },
         'fileType' : 'tempfileInfo',
         'fileSection' : 'recordSection',
@@ -150,9 +241,8 @@ app.post('/uploadTest', upload.single('file'), (req, res) => {
 
 
     console.log('file HAS been added');
-    res.redirect('/home');
-})
-
+    // res.redirect('/home');
+});
 
 
 
@@ -186,19 +276,8 @@ conn.once('open', function() {
 }
 */
 
-app.use(express.static(path.join(__dirname, 'public')))
 
 
-// Set favicon to the SST crest
-app.use(favicon(__dirname + '/public/images/logo.png'));
-
-app.set('view engine', 'ejs'); // set up ejs for templating
-
-// required for passport
-app.use(session({ secret: 'dfgd5155435445df1gdfgdry5y4345' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
 
 require('./app/routes/index.js')(app, passport);
 
