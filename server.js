60d92e159a054ce1aa87f6f164e9f84449d4bb81
ekim22 <ekim2@ggc.edu>
2019-11-12 19:24:34 -0500

    Implemented downloading as original file name (e.g. 'sample.txt').

--- server.js
+++ server.js
@@ -364,11 +364,9 @@ app.post('/uploadBioOther', upload.single('bioOther'), (req, res) => {
     res.redirect('/home');
 });
 
-app.get('/getFiles/:filename', (req, res) => {
+app.get('/getFiles/:filename/:documentName', (req, res) => {
+    console.log("Doc name:" + req.params.documentName);
     gfs.files.findOne({filename : req.params.filename}, (err, file) => {
-        console.log("Req params: " + req.params);
-        console.log("Req params filename: " + req.params.filename);
-        console.log("Req params fileId: " + req.params.fileId);
 
         if (!file || file.length === 0) {
             return res.status(404).json({
